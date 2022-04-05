18a01bcadfa11f1172d940339e047ad07245fed4
natcobbinah <baby.hugo@yahoo.com>
2022-04-05 21:49:39 +0200

    code refactored and object passed as props from parent component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -13,12 +13,11 @@ class BusinessList extends Component {
     render() {
         return (
             <div className="BusinessList">
-                <Business/>
-                <Business/>
-                <Business/>
-                <Business/>
-                <Business/>
-                <Business/>
+                {
+                    this.props.businesses.map(business => {
+                      return  <Business business = { business }/>
+                    })
+                }
             </div>
         );
     }
