0048121d349ed60514a28185a383ce56564a07cf
natcobbinah <baby.hugo@yahoo.com>
2022-04-11 20:36:18 +0200

    redundant class comp refactored to fxntal components

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,37 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Business.css';
 
 
-class Business extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-
-    render() {
+const Business = (props) => {
         return (
             <div className="Business">
                 <div className="image-container">
-                    <img src={this.props.business.imageSrc} alt='business Image' />
+                    <img src={props.business.imageSrc} alt='business Image' />
                 </div>
-                <h2>{this.props.business.name}</h2>
+                <h2>{props.business.name}</h2>
                 <div className="Business-information">
                     <div class="Business-address">
-                        <p>{this.props.business.address}</p>
-                        <p>{this.props.business.city}</p>
-                        <p>{this.props.business.zipCode}</p>
+                        <p>{props.business.address}</p>
+                        <p>{props.business.city}</p>
+                        <p>{props.business.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{this.props.business.category}</h3>
-                        <h3 className="rating">{this.props.business.rating}</h3>
-                        <p>{this.props.business.reviewCount}</p>
+                        <h3>{props.business.category}</h3>
+                        <h3 className="rating">{props.business.rating}</h3>
+                        <p>{props.business.reviewCount}</p>
                     </div>
                 </div>
             </div>
         );
-    }
 }
 
 export default Business;
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
-class BusinessList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-    }
-
-    render() {
-        return (
-            <div className="BusinessList">
-                {
-                    this.props.businesses.map(business => {
-                      return  <Business key={business} business = { business }/>
-                    })
-                }
-            </div>
-        );
-    }
+const BusinessList = (props) => {
+    return (
+        <div className="BusinessList">
+            {
+                props.businesses.map(business => {
+                    return <Business key={business} business={business} />
+                })
+            }
+        </div>
+    );
 }
 
 export default BusinessList;
