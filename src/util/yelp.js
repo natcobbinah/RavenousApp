773a04e3f21e9dee5c74eca054d6fca93eb255a3
natcobbinah <baby.hugo@yahoo.com>
2022-12-15 23:22:09 +0100

    cors error resolved

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -1,15 +1,19 @@
 const apiKey = "iJT8uvomB80kzqDs-VVBd7_vKM05kSJFODvrdrYY2chg_bks5yHAQqzIUhTYNKf9G_K1QqzPKRQ3w0a_xLLlPBSb70w2GNWitW0_IrB_Tt4X51J6Estajieh32BQYnYx";
 const businessesBaseUrl = "https://api.yelp.com/v3/businesses/search?";
-const corsAnyWhereHerokuBuildUrl = "https://enigmatic-gorge-40032.herokuapp.com/";
+//const corsAnyWhereHerokuBuildUrl = "https://enigmatic-gorge-40032.herokuapp.com/";
+const corsAnyWhereHerokuBuildUrl = "https://cors-anywhere.herokuapp.com/";
 
 const search = async (term, location, sortBy) => {
     const response = await fetch(`${corsAnyWhereHerokuBuildUrl}${businessesBaseUrl}term=${term}&location=${location}&sort_by=${sortBy}`,
         {
+
             headers: {
                 Authorization: `Bearer ${apiKey}`,
             }
         });
     const jsonResponse = await response.json();
+    console.log(jsonResponse);
+
     if (jsonResponse.businesses) {
         return jsonResponse.businesses.map(business => ({
             id: business.id,
